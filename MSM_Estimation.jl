eecbcc52c7401a1b9f15d8ceb7a0a1a7009e68ae
larsspreng <larsspreng@outlook.de>
2022-11-29 14:01:03 +0000

    implemented ll estimation

--- MSM_Estimation.jl
+++ MSM_Estimation.jl
@@ -8,6 +8,7 @@ using TimeSeries
 using LinearAlgebra 
 using BenchmarkTools
 using Bits
+using LoopVectorization
 data = vec(Array(DataFrame(CSV.File("data.csv"))));
 
 kbar=3;
@@ -16,7 +17,7 @@ function estimate(
     msm::MSMStructure,
 
 )
-    
+    @btime likelihood(input,kbar,data)
 end
 
 
@@ -26,61 +27,72 @@ function likelihood(
     data,
 )
     
-    sigma = input[4]/sqrt(252);
+    σ = input[4]/sqrt(252);
     kbar2 = 2^kbar;
-    kbar1 = kbar + 1;
     T = size(data,1);                       
 
-    A = transition_mat(input,kbar,kbar1,kbar2); # Compute state transition matrix
-    g_m = Vector{Float64}(undef,kbar2)
-    gofm(g_m,input,kbar,kbar2); # Compute all possible states 
+    A = transition_mat(input,kbar,kbar2); # Compute state transition matrix
+    g_m = ones(kbar2) #Vector{Float64}(undef,kbar2)
+    gofm(g_m,input,kbar); # Compute all possible states 
     wt = Matrix{Float64}(undef,kbar2,T);
-    @btime get_weights!(wt,data,g_m,sigma,T,kbar2);
+    get_weights!(wt,data,g_m,σ);
     
     pi_mat = Matrix{Float64}(undef,kbar2,T+1);     
     pi_mat[:,1] .= (1/kbar2);
-    @btime get_loglik!(pi_mat,A,wt,T)
-    
+    ll = get_loglik!(pi_mat,A,wt)
+    return -ll
 end
 
 function get_loglik!(
     pi_mat,
     A,
     wt,
-    T,
 )
     ll = 0.0;
-    @views for t=2:T+1          
-        piA = A*pi_mat[:,t-1];
-        C = wt[:,t-1].*piA; 
-        ft = simplesum(C);
+    piA = similar(wt[:,1])
+    @views @inbounds for t in axes(wt,2)          
+        mul!(piA,A,pi_mat[:,t]);
+        pi_mat[:,t+1] .= wt[:,t].*piA; 
+        ft = sum_tturbo(pi_mat[:,t+1]);
         if ft == 0                    
-            pi_mat[1,t] = 1.0;   
+            pi_mat[1,t+1] = 1.0;   
         else
-            pi_mat[:,t] = C./ft; 
+            pi_mat[:,t+1] ./= ft; 
         end
         
-        ll += log(wt[:,t-1]*piA);
+        ll += log(dot_tturbo(wt[:,t],piA));
     end
+    return ll
 end
 
 function get_weights!(
     wt,
     data,
     g_m,
-    sigma,
-    T,
-    kbar2
+    σ,
 )
-    pa = (2*pi)^-0.5;
-    @views @inbounds for i in 1:T 
-        for j in 1:kbar2
-            wt[j,i] = pa*exp(- 0.5*( (data[i]/(sigma*g_m[j]))^2 ))/(sigma*g_m[j]) + 1e-16;
+    pa = (2.0*pi)^-0.5;
+    @views @inbounds @fastmath for i in axes(wt,2)
+        for j in axes(wt,1)
+            wt[j,i] = pa*exp(- 0.5*( (data[i]/(σ*g_m[j]))^2 ))/(σ*g_m[j]) + 1e-16;
         end
     end
 end
 
-function transmat_template(A,kbar::Int64)  
+function transition_mat(input,kbar::Int64,kbar2::Int64)
+    A = zeros((kbar2),(kbar2));
+    transmat_template(A,kbar);
+    
+    gamma = Matrix{Float64}(undef,2,kbar);                          
+    get_gammas!(gamma,input,kbar,) 
+
+    prob = ones(kbar2);    
+    get_probs!(prob,gamma,kbar)
+    get_transmat!(A,prob,kbar,kbar2)
+    return A
+end
+
+function transmat_template(A::Matrix{Float64},kbar::Int64)  
     @views @inbounds for i in 0:(2^kbar-1)       
         for j = i:(2^kbar-1)-i  
             A[i+1,j+1] = xor(i,j);
@@ -89,45 +101,33 @@ function transmat_template(A,kbar::Int64)
     return A
 end
 
-function transition_mat(input,kbar,kbar1,kbar2)
-    A = zeros((2^kbar),(2^kbar));
-    transmat_template(A,kbar);
-    b = input[1];
-    gamma_kbar = input[3];
-    
-    gamma = Matrix{Float64}(undef,2,kbar);                          
-    get_gammas!(gamma,gamma_kbar,b,kbar,) 
-    prob = ones(kbar2);    
-    get_probs!(prob,gamma,kbar,kbar1)
-    get_transmat!(A,prob,kbar,kbar2,)
-    return A
-end
-
 function get_gammas!(
-    gamma,
-    gamma_kbar,
-    b,
+    γ,
+    input,
     kbar,
 )
-    gamma[1,1] = 1-(1-gamma_kbar)^(1/(b^(kbar-1)));
-    gamma[2,1] = (1-(1-gamma_kbar)^(1/(b^(kbar-1))))*0.5;
-    @views for i in 2:kbar
-        gamma[1,i] = 1-(1-(1-gamma[1,1])^(b^(i-1)))*0.5;
-        gamma[2,i] = (1-(1-gamma[1,1])^(b^(i-1)))*0.5;
+    
+    @views @inbounds for i in axes(γ,2)
+        if i == 1
+            γ[1,1] = 1.0-(1.0-input[3])^(1.0/(input[1]^(kbar-1)));
+            γ[2,1] = (1.0-(1.0-input[3])^(1.0/(input[1]^(kbar-1))))*0.5;
+        else
+            γ[1,i] = 1.0-(1.0-(1.0-γ[1,1])^(input[1]^(i-1)))*0.5;
+            γ[2,i] = (1.0-(1.0-γ[1,1])^(input[1]^(i-1)))*0.5;
+        end
     end
-    gamma[1,1] *= (-0.5);
-    gamma[1,1] += 1.0
+    γ[1,1] *= (-0.5);
+    γ[1,1] += 1.0
 end
 
 function get_probs!(
     prob,
     gamma,
     kbar,
-    kbar1
 )
-    @views for i=0:2^kbar-1   
+    @views @inbounds @fastmath for i in eachindex(prob)   
         for m = 1:kbar  
-            prob[i+1] *= gamma[(bit(i,m)+1),kbar1-m];
+            prob[i] *= gamma[(bit(i-1,m)+1),kbar+1-m];
         end
     end
 end
@@ -152,29 +152,33 @@ function get_transmat!(
     end 
 end
 
-function gofm(g_m,input,kbar,kbar2)
-    m0 = input[2]; 
-    m1 = 2.0-m0;
-    g_m1 = collect(0:(kbar2-1));
-    @views for i in 1:(kbar2)
-        g=1.0;
+function gofm(g_m,input,kbar)
+    @views @inbounds for i in eachindex(g_m)
         for j in 0:(kbar-1)       
-            if g_m1[i] == 2^j   
-                g *= m1;
+            if i-1 == 2^j   
+                g_m[i] *= (2.0 - input[2]);
             else 
-                g *= m0;
+                g_m[i] *= input[2];
             end
         end
-        g_m[i] = sqrt(g);
+        g_m[i] ^= 0.5;
     end
 end
 
-function simplesum(x::AbstractArray)   
-    out = 0.0
-    @views @inbounds for i in axes(x,1)
-        for j in axes(x,2)
-            out += x[i,j]
+function sum_tturbo(x::AbstractArray{T}) where {T <: Real} 
+    s = zero(T)
+    @tturbo for j ∈ axes(x,2)
+            for i ∈ axes(x,1)
+            s += x[i,j]
         end
     end
-    return out
+    s
+end
+
+function dot_tturbo(a::AbstractArray{T}, b::AbstractArray{T}) where {T <: Real}
+    s = zero(T)
+    @tturbo for i ∈ eachindex(a,b)
+        s += a[i] * b[i]
+    end
+    s
 end
